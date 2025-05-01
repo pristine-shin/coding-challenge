aa607f114ca8d3ef35c72c5f84ad15067c188a35
Pristine Shin <157057129+pristine-shin@users.noreply.github.com>
2025-04-30 18:46:22 -0700

    Update subSum.js

--- exercises/subSum.js
+++ exercises/subSum.js
@@ -11,11 +11,19 @@ module.exports.run = function(x, y) {
 
 	 Write your code below the comment.
 */
-const twoDArray = [[0,  4,  2,  1],
-		   [3, -1,  1,  0],
-		   [0,  0,  2,  3],
-		   [1,  3, -3,  0]];
-
+	const twoDArray = [[0,  4,  2,  1],
+			   [3, -1,  1,  0],
+			   [0,  0,  2,  3],
+			   [1,  3, -3,  0]];
+	
+	let sum = 0;
+	
+	for (let row = 0; row <= y; row++) {
+		for (let col = 0; col <= x; col++) {
+			sum += twoDArray[row][col];
+		};
+	};
+	return sum;
 };
 
 
